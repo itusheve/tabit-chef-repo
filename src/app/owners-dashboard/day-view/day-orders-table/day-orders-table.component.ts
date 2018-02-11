f638fcea61dbf69f0927ad6ff428c7b218f7f34a
or <or@tabit.cloud>
2018-02-11 18:31:56 +0200

    wip

--- src/app/owners-dashboard/day-view/day-orders-table/day-orders-table.component.ts
+++ src/app/owners-dashboard/day-view/day-orders-table/day-orders-table.component.ts
@@ -1,13 +1,16 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { DatePipe } from '@angular/common';
+
 import { Order } from '../../../../tabit/model/Order.model';
 import { DataService } from '../../../../tabit/data/data.service';
+
 
 import * as _ from 'lodash';
 
 export interface OrderTypeVM {
   id: string;
   caption: string;
-  orders: Order[];
+  orders: any[];
   sales: number;
 }
 
@@ -22,13 +25,23 @@ export class DayOrdersTableComponent implements OnInit {
 
   public byOrderTypes: OrderTypeVM[];
 
+  datePipe: DatePipe = new DatePipe('he-IL');
+
   constructor(private dataService: DataService) {}
   
   ngOnInit() {
     const orderTypes = _.cloneDeep(this.dataService.orderTypes);
     orderTypes.forEach(ot=>{
-      ot.orders = this.orders.filter(o=>o.orderTypeId===ot.id);
-      ot.sales = ot.orders.reduce((acc, curr)=>acc+(curr.sales || 0),0);      
+      ot.orders = this.orders.filter(o => o.orderTypeId === ot.id).sort((a, b) => a.number < b.number ? -1 : 1);
+      ot.sales = ot.orders.reduce((acc, curr) => acc + (curr.sales || 0), 0);            
+      // ot.orders = this.orders.filter(o=>o.orderTypeId===ot.id).map(o=>({
+      //   id: o.id,
+      //   openingTime: o.openingTime.format('H:mm'),
+      //   number: o.number,
+      //   waiter: o.waiter,
+      //   sales: o.sales
+      // })).sort((a,b)=>a.number<b.number ? -1 : 1);
+      // ot.sales = ot.orders.reduce((acc, curr)=>acc+(curr.sales || 0),0);      
     });
     this.byOrderTypes = orderTypes;
 
