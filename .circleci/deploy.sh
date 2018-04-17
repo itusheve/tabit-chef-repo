a13a0ee8c55d33333b8eed9b2ed50d9cf62c8e33
shmuel <moly.witman@gmail.com>
2018-04-17 12:27:08 +0300

    revert gen push

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -26,9 +26,8 @@ elif [[ "$CIRCLE_BRANCH" == "ecs-dev" ]]; then
   npm install --ignore-scripts
   npm rebuild node-sass
   ng build --aot --env=prod-il --target=production --output-hashing=none --i18nFile=src/locale/messages.he.xlf --i18nFormat=xlf --locale=he
-  IAM_ROLE_ARN="arn:aws:iam::101444535047:role/allow-auto-deploy-from-other-accounts"
-  chmod +x _s3/s3-push.sh
-  S3_BUCKET="chef.tabit-dev.com" _s3/s3-push.sh
+  chmod +x _s3/s3-push-dev.sh
+  S3_BUCKET="chef.tabit-dev.com" _s3/s3-push-dev.sh
   echo "On $CIRCLE_BRANCH branch. Deploying $SERVICES to $ACCOUNT in $REGIONS"
 elif [[ "$CIRCLE_BRANCH" == "us-production-rp" ]]; then
   npm rebuild node-sass
