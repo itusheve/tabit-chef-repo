a44ca7e4eaaf72fdc2f51c4e66992039af149b90
Assaf <assafwint@gmail.com>
2018-10-11 18:10:03 +0300

    month picker

--- src/app/owners-dashboard/month-view/month-selector/month-picker-dialog.component.ts
+++ src/app/owners-dashboard/month-view/month-selector/month-picker-dialog.component.ts
@@ -4,11 +4,14 @@ import {MAT_BOTTOM_SHEET_DATA} from '@angular/material';
 import * as moment from 'moment';
 import * as _ from 'lodash';
 import {DataService} from '../../../../tabit/data/data.service';
+import {TabitHelper} from '../../../../tabit/helpers/tabit.helper';
+import { DatePipe } from '@angular/common';
 
 @Component({
     selector: 'app-month-selector-dialog',
     templateUrl: 'month-picker-dialog.html',
-    styleUrls: ['month-picker-dialog.css']
+    styleUrls: ['month-picker-dialog.scss'],
+    providers: [DatePipe]
 })
 export class MonthPickerDialogComponent {
 
@@ -16,17 +19,79 @@ export class MonthPickerDialogComponent {
     selection: moment.Moment;
     months: any;
     onDateChanged: any;
+    tabitHelper;
 
     constructor(
+        private datePipe: DatePipe,
         private bottomSheetRef: MatBottomSheetRef<MonthPickerDialogComponent>,
         @Inject(MAT_BOTTOM_SHEET_DATA) data,
         private dataService: DataService) {
 
+        this.tabitHelper = new TabitHelper();
         this.onDateChanged = data.onDateChanged;
         this.currentMonth = data.selected;
         this.dataService.databaseV2$.subscribe(database => {
             this.months = [];
             _.each(database._data, month => {
+
+                let summaryCardData = {
+                    loading: false,
+                    title: this.datePipe.transform(month.latestDay, 'MMMM yyyy'),
+                    tag: '',
+                    sales: 0,
+                    diners: 0,
+                    ppa: 0,
+                    aggregations: {},
+                    averages: {weekly: {percentage: 0, change: 0}},
+                    reductions: {},
+                    statusClass: ''
+                };
+
+                let previousMonth = database.getMonth(moment(month.latestDay).subtract(1,'months'));
+
+                summaryCardData.diners = month.diners || month.orders;
+                summaryCardData.ppa = month.ppaIncludeVat;
+                summaryCardData.sales = month.ttlsalesIncludeVat;
+
+                summaryCardData.averages = {
+                    /*yearly: {
+                        percentage: month.aggregations.sales.lastYearWeekAvg ? ((month.aggregations.sales.weekAvg / month.aggregations.sales.lastYearWeekAvg) - 1) : 0,
+                        change: month.aggregations.sales.weekAvg / month.aggregations.sales.lastYearWeekAvg
+                    },*/
+                    weekly: {
+                        percentage: previousMonth && previousMonth.weekAvg ? ((month.weekAvg / previousMonth.weekAvg) - 1) : 0,
+                        change: previousMonth ? (month.weekAvg / previousMonth.weekAvg) * 100: 0
+                    }
+                };
+
+                summaryCardData.reductions = {
+                    cancellations: {
+                        percentage: month.prcVoidsAmount / 100,
+                        change: previousMonth ? (month.prcVoidsAmount / previousMonth.prcVoidsAmount * 100) : 0
+                    },
+                    employee: {
+                        percentage: month.prcEmployeesAmount / 100,
+                        change: previousMonth ? (month.prcEmployeesAmount / previousMonth.prcEmployeesAmount * 100) : 0
+                    },
+                    operational: {
+                        percentage: month.prcOperationalAmount / 100,
+                        change: previousMonth ? (month.prcOperationalAmount / previousMonth.prcOperationalAmount * 100) : 0
+                    },
+                    retention: {
+                        percentage: month.prcMrAmount / 100,
+                        change: previousMonth ? (month.prcMrAmount / previousMonth.prcMrAmount * 100) : 0
+                    }
+                };
+
+                if (summaryCardData.averages.weekly.percentage) {
+                    let value = previousMonth ? (month.weekAvg / previousMonth.weekAvg) * 100 : 0;
+                    summaryCardData.statusClass = this.tabitHelper.getColorClassByPercentage(value, true);
+                }
+                else {
+                    summaryCardData.statusClass = '';
+                }
+
+                summaryCardData.loading = false;
 
                 if(month.latestDay) {
                     let date = moment(month.latestDay);
@@ -40,7 +105,8 @@ export class MonthPickerDialogComponent {
                         date: moment(month.latestDay),
                         sales: month.ttlsalesIncludeVat,
                         salesWithoutVat: month.ttlsalesExcludeVat,
-                        selected: selected
+                        selected: selected,
+                        summary: summaryCardData
                     });
 
                     this.months = _.orderBy(this.months, 'date', 'desc');
