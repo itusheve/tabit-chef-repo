c6233f40004e6b6d352bec312cd0b431704552e9
itusheve <itamar@tabit.cloud>
2019-02-25 15:52:35 +0200

    added holidays in names

--- src/app/owners-dashboard/home/forecast-dialog/forecast-dialog.component.ts
+++ src/app/owners-dashboard/home/forecast-dialog/forecast-dialog.component.ts
@@ -23,26 +23,29 @@ export class ForecastDialogComponent implements OnInit {
         this.env = environment;
         this.todayDate = moment().format('YYYY-MM-DD');
         combineLatest(dataService.databaseV2$, dataService.vat$)
-        .subscribe(data => {
-            let database = data[0];
-            let vat = data[1];
-            this.incVat = vat;
-            let month = database.getCurrentMonth();
-            this.forecast = _.get(month, ['forecast', 'sales', 'amount'], []);
-            let days = _.get(month, ['forecast', 'days'], []);
-            days = _.orderBy(days, function (day) {
-                return new Date(day.date);
-            },'desc');
+            .subscribe(data => {
+                let database = data[0];
+                let vat = data[1];
+                this.incVat = vat;
+                let month = database.getCurrentMonth();
+                this.forecast = _.get(month, ['forecast', 'sales', 'amount'], []);
+                let days = _.get(month, ['forecast', 'days'], []);
+                _.forEach(days, day => {
+                    _.set(day, 'holiday', _.get(database.getDay(moment(day.date)), 'holiday'));
+                });
+                days = _.orderBy(days, function (day) {
+                    return new Date(day.date);
+                },'desc');
 
-            this.vat = vat ? 1 : month.vat;
-            this.days = days;
-        });
+                this.vat = vat ? 1 : month.vat;
+                this.days = days;
+            });
     }
 
     ngOnInit() {
     }
 
     getAmount(amount): number {
-      return amount / this.vat;
+        return amount / this.vat;
     }
 }
