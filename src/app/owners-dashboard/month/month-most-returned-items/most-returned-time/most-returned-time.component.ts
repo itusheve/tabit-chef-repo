d3efff6da2486e48581d3c7c2182909848aaa52c
itusheve <itamar@tabit.cloud>
2019-03-31 13:58:50 +0300

    fix ui

--- src/app/owners-dashboard/month/month-most-returned-items/most-returned-time/most-returned-time.component.ts
+++ src/app/owners-dashboard/month/month-most-returned-items/most-returned-time/most-returned-time.component.ts
@@ -13,11 +13,11 @@ export class MostReturnedTimeComponent extends AbstractTableComponent implements
 
   columns = {
     primary: [
-      {en: 'Department', dataKey: 'departmentName', translated: 'day.department'},
-      {en: 'Item', dataType: 'item', dataKey: 'itemName', translated: 'day.item'},
-      {en: 'Prepared', dataType: 'number', dataKey: 'prepared', translated: 'month.prepared'},
-      {en: 'Returned', dataKey: 'return', additionalDataKey:'prc', dataType: 'number', translated: 'month.returned'},
-      {en: 'OperationalReductionsValue',  dataKey: 'returnAmount',  dataType: 'currency', translated: 'month.operationalReductionsValue'}
+      {en: 'Department', dataKey: 'departmentName', translated: 'day.department', width:'20%'},
+      {en: 'Item', dataType: 'item', dataKey: 'itemName', translated: 'day.item', width:'20%'},
+      {en: 'Prepared', dataType: 'number', dataKey: 'prepared', translated: 'month.prepared', width:'20%'},
+      {en: 'Returned', dataKey: 'return', additionalDataKey:'prc', dataType: 'number', translated: 'month.returned', width:'20%'},
+      {en: 'OperationalReductionsValue',  dataKey: 'returnAmount',  dataType: 'currency', translated: 'month.operationalReductionsValue', width:'20%'}
 
     ],
     alt:[]
